13aa3e5f964ae13e3a67f0b03b14e939e07f1224
alekefromkz <kydyrmin.almaz@gmail.com>
2023-12-16 14:24:58 +0200

    Create employees app with models and admins

---
+++ offisync/employees/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+
+class Employee(models.Model):
+    employee_id = models.CharField(max_length=20, primary_key=True, unique=True)
+    first_name = models.CharField(max_length=24)
+    last_name = models.CharField(max_length=24)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+    class Meta:
+        verbose_name = "Employee"
+        verbose_name_plural = "Employees"
+
+
+class WorkHistory(models.Model):
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    office = models.ForeignKey("offices.Office", on_delete=models.CASCADE)
+    start_date = models.DateField()
+    end_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.employee} - {self.office}"
+
+    class Meta:
+        verbose_name = "Work history"
+        verbose_name_plural = "Work histories"
--- offisync/offisync/settings.py
+++ offisync/offisync/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     # local
     'offices',
+    'employees',
 
     # custom installed
     'cities_light',
